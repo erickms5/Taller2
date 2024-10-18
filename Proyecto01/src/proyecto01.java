474fd6c30d8fce0439d633d5a4a3c9e9d1fbd6a0
erickms5 <erick.emelec.m@gmail.com>
2024-10-17 19:56:36 -0500

    primer commit

---
+++ Proyecto01/src/proyecto01.java
@@ -0,0 +1,7 @@
+public class proyecto01 {
+
+    public static void main(String[] args) {
+        System.out.println("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
+    }
+    
+}
