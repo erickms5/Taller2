8d3c60007adf20b241899a2074457bf829caf90d
erickms5 <erick.emelec.m@gmail.com>
2024-10-17 20:13:20 -0500

    tercer commit, trabajando con Git y GitHubClassroom

---
+++ Proyecto02/src/Proyecto02.java
@@ -0,0 +1,7 @@
+public class Proyecto02 {
+
+    public static void main(String[] args) {
+        System.out.println("Ejemplo dos básico de Java Versionado a través de Git Manejado desde GitHub Además se usar GitHubClassroom");
+    }
+    
+}
